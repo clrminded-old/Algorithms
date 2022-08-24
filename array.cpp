176c1c64ac88135585aef451ecb2b8aa18b405d2
clrminded <the_rocket_scientist@icloud.com>
2022-08-23 23:36:18 -0700

    adding a beginner vector code to start off with

---
+++ array.cpp
@@ -0,0 +1,14 @@
+// This is a file with all the information and notes that I have collected on arrays.
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> v = {1, 2, 3, 4, 5};
+
+    for (auto i : v)
+        std::cout << i << " ";
+
+    return 0;
+}
